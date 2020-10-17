315fff358d18a25cfad953e4314c78469482cb10
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-10-16 22:32:08 -0400

    adding more files

---
+++ day_one_homework/unsolved/homework_one.js
@@ -0,0 +1,13 @@
+/*
+Assignment:
+
+1. Write a script that prompts the user to input a value.
+2. If the prompt input value is less than 100, log to the user of what the value was  and that the value is less than 100.
+3. If the prompt input value is not less than 100, log to the user of what the value was and that it was greater than 100.
+4. If the prompt input value equals 100, alert the user that 100 is the winning number.
+    - The syntax for "alert" is easy. Just replace console.log with alert
+
+Bonus (Done while teaching exercise):
+1. Check that the prompt input value is an integer
+
+*/
